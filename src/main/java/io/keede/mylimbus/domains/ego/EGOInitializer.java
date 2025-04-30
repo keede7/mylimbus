6f2f1344bd23fb97f5b855d86992022b9274a497
keede <wesaq11@gmail.com>
2025-04-30 17:39:26 +0900

    EGO entity #2

---
+++ src/main/java/io/keede/mylimbus/domains/ego/EGOInitializer.java
@@ -0,0 +1,12 @@
+package io.keede.mylimbus.domains.ego;
+
+
+/**
+ * @author keede
+ * Created on 2025/04/30
+ */
+public class EGOInitializer {
+
+
+
+}
--- src/main/java/io/keede/mylimbus/domains/ego/entity/EGO.java
+++ src/main/java/io/keede/mylimbus/domains/ego/entity/EGO.java
@@ -1,15 +1,32 @@
 package io.keede.mylimbus.domains.ego.entity;
 
 import io.keede.mylimbus.domains.personality.entity.AttackType;
+import io.keede.mylimbus.domains.personality.entity.Sin;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author keede
  * Created on 2025/04/29
  */
+@Entity
+@Getter
+@DynamicUpdate
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = {"egoUseConditions"})
 public class EGO {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String characterName;
@@ -23,8 +40,9 @@ public class EGO {
 
     private AttackType attackType;
 
-    // 필요한 자원
-    private Map<String, Integer> useCondition;
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "ego_id")
+    private List<EGOUseCondition> egoUseConditions;
 
     public EGO(
             String characterName,
@@ -32,13 +50,13 @@ public class EGO {
             String imgUrl,
             RiskLevel riskLevel,
             AttackType attackType,
-            Map<String, Integer> useCondition
+            List<EGOUseCondition> egoUseConditions
     ) {
         this.characterName = characterName;
         this.egoName = egoName;
         this.imgUrl = imgUrl;
         this.riskLevel = riskLevel;
         this.attackType = attackType;
-        this.useCondition = useCondition;
+        this.egoUseConditions = egoUseConditions;
     }
 }
