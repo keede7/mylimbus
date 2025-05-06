6ad5480e9e75547317d0528740d5a7afb47192ef
keede <wesaq11@gmail.com>
2025-05-06 15:00:14 +0900

    Modal event

--- src/main/resources/static/components/js/main.js
+++ src/main/resources/static/components/js/main.js
@@ -12,16 +12,16 @@ function openCharacterModal(characterElement) {
 }
 
 // Function to close character selection modal
-function closeCharacterModal() {
-
-    document.querySelectorAll('input[type="checkbox"]')
-        .forEach(checkbox => {
-            checkbox.checked = false;
-        });
-
-    const modal = document.getElementById('characterModal');
-    modal.style.display = 'none';
-}
+// function closeCharacterModal() {
+//
+//     document.querySelectorAll('input[type="checkbox"]')
+//         .forEach(checkbox => {
+//             checkbox.checked = false;
+//         });
+//
+//     const modal = document.getElementById('characterModal');
+//     modal.style.display = 'none';
+// }
 
 // personalityId 미사용
 function fetchAlternativeCharacters(personalityId, baseNameKor) {
@@ -252,14 +252,20 @@ document.querySelectorAll('.character-card').forEach(card => {
     });
 });
 
-// Close modal when clicking outside of it
-window.onclick = function (event) {
-    const modal = document.getElementById('characterModal');
-    console.log(event.target === modal)
-    if (event.target === modal) {
-        closeCharacterModal();
-    }
-};
+// // Close modal when clicking outside of it
+// window.onclick = function (event) {
+//     const modal = document.getElementById('characterModal');
+//     console.log(event.target === modal)
+//     if (event.target === modal) {
+//         closeCharacterModal();
+//     }
+//
+//     const egoModal = document.getElementById('egoModal');
+//     if (event.target === egoModal) {
+//         egoModal.style.display = 'none';
+//     }
+//
+// };
 
 function filterCharacters() {
     // Get all selected keywords
---
+++ src/main/resources/static/components/js/modal.js
@@ -0,0 +1,26 @@
+// Function to close character selection modal
+function closeCharacterModal() {
+
+    document.querySelectorAll('input[type="checkbox"]')
+        .forEach(checkbox => {
+            checkbox.checked = false;
+        });
+
+    const modal = document.getElementById('characterModal');
+    modal.style.display = 'none';
+}
+
+// Close modal when clicking outside of it
+window.onclick = function (event) {
+    const modal = document.getElementById('characterModal');
+    console.log(event.target === modal)
+    if (event.target === modal) {
+        closeCharacterModal();
+    }
+
+    const egoModal = document.getElementById('egoModal');
+    if (event.target === egoModal) {
+        egoModal.style.display = 'none';
+    }
+
+};
