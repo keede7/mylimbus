0f3fa0eb8c2a2452bfb0112c9552ee5795800715
keede <wesaq11@gmail.com>
2025-05-06 15:22:29 +0900

    fix main page

--- src/main/java/io/keede/mylimbus/web/ViewController.java
+++ src/main/java/io/keede/mylimbus/web/ViewController.java
@@ -3,16 +3,16 @@ package io.keede.mylimbus.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+* @author keede
+* Created on 2025/05/06
+*/
 @Controller
 public class ViewController {
 
-    @GetMapping("/main")
-    public String main() {
+    @GetMapping("")
+    public String mainTest() {
         return "main";
     }
 
-    @GetMapping("/mainTest")
-    public String mainTest() {
-        return "mainTest";
-    }
 }
