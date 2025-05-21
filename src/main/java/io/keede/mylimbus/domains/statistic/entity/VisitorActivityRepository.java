5a52e86408ea4e03b305a8c8de8f3c280244685f
keede <wesaq11@gmail.com>
2025-05-21 17:26:23 +0900

    기본 엔티티 추가

---
+++ src/main/java/io/keede/mylimbus/domains/statistic/entity/VisitorActivityRepository.java
@@ -0,0 +1,11 @@
+package io.keede.mylimbus.domains.statistic.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author keede
+ * Created on 2025/05/21
+ */
+public interface VisitorActivityRepository extends JpaRepository<VisitorActivity, Long> {
+
+}
