89b30906ffa6c207c788959fc572279a8d15499e
keede <wesaq11@gmail.com>
2025-06-24 11:22:02 +0900

    Update to enable Mobile when landscape mode

--- src/main/java/io/keede/mylimbus/web/ViewController.java
+++ src/main/java/io/keede/mylimbus/web/ViewController.java
@@ -33,14 +33,14 @@ public class ViewController {
             HttpServletRequest request,
             HttpServletResponse response
     ) {
-        String userAgent = request.getHeader("User-Agent");
-        String deviceType = MobileValidator.getDeviceType(userAgent);
-
-        if ("mobile".equals(deviceType) || "tablet".equals(deviceType)) {
-            return "mobile-not-supported";
-        }
+//        String userAgent = request.getHeader("User-Agent");
+//        String deviceType = MobileValidator.getDeviceType(userAgent);
 
         this.recordVisitorActivity(request, response);
+
+//        if ("mobile".equals(deviceType) || "tablet".equals(deviceType)) {
+//            return "mobile-not-supported";
+//        }
 
         return "main";
     }
