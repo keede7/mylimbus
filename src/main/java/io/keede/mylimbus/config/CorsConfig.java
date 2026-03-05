4edb428790820f3c0924fc4356bccb4a81844922
keede <wesaq11@gmail.com>
2026-03-05 18:39:06 +0900

    feat: add Cors Config

---
+++ src/main/java/io/keede/mylimbus/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package io.keede.mylimbus.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+* @author keede
+* Created on 2026. 3. 5.
+*/
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*"); // Flutter 앱 허용
+    }
+}
